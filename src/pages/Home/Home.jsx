b74c860b03fa21162634130afa20fbdb29003f3f
ravishka1325 <ravishka.1325@gmail.com>
2024-12-18 14:19:48 +0530

    created the routing basics and home page iitems

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/Header/Header' // Adjust the import path as needed
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
