1d00f73c32f7f3a22d6a3e1f56e37e2cd0f5ba33
ravishka1325 <ravishka.1325@gmail.com>
2024-12-18 10:53:35 +0530

    #error ammended regarding the non aligned icons in | Navbar.jsx

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
         <img src={assets.search_icon} alt=''/>
         <div className="navbar-search-icon">
             <img src={assets.basket_icon} alt=''/>
-            <div className="dot">
+            <div className="dot"></div>
 
-            </div>
-            <button>sign-in</button>
+            
         </div>
+        <button>sign-in</button>
       </div>
     </div>
   )
